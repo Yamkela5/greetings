3bc94a84d32203d4dde9310971b1624ceda99fe7
Yamkela <owamjames1@gmail.com>
2017-03-09 16:33:46 +0200

    adding a file

---
+++ greet.js
@@ -0,0 +1,54 @@
+var clickCount = Number(localStorage.getItem('currentGreets'));
+document.getElementById('greetCount').innerHTML = clickCount;
+
+var namesGreeted = {};
+
+for (var i = 0; i < namesGreeted.length; i++) {
+        }
+
+
+function myName() {
+    'use strict';
+
+    var Yash = document.getElementById('TextBox').value;
+
+    if (document.getElementById('English').checked === true && Yash.length > 0 && namesGreeted[Yash] === undefined) {
+        namesGreeted[Yash] = 1;
+        document.getElementById('output').innerHTML = 'Hello, ' + Yash;
+
+        clickCount += 1;
+
+  } else if (document.getElementById('TshiVenda').checked === true && Yash.length > 0 && namesGreeted[Yash] === undefined) {
+        namesGreeted[Yash] = 1;
+        document.getElementById('output').innerHTML = 'Ndaa, ' + Yash;
+
+        clickCount += 1;
+
+  } else if (document.getElementById('French').checked === true && Yash.length > 0 && namesGreeted[Yash] === undefined) {
+        namesGreeted[Yash] = 1;
+        document.getElementById('output').innerHTML = 'Bonjour, ' + Yash;
+
+        clickCount += 1;
+    } else if (document.getElementById('English').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
+        document.getElementById('output').innerHTML = 'Hello, ' + Yash;
+    } else if (document.getElementById('TshiVenda').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
+        document.getElementById('output').innerHTML = 'Ndaa, ' + Yash;
+    } else if (document.getElementById('French').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
+        document.getElementById('output').innerHTML = 'Bonjour, ' + Yash;
+    }
+
+    document.getElementById('TextBox').value = "";
+
+    document.getElementById('greetCount').innerHTML = clickCount;
+
+    localStorage.setItem('currentGreets',  Number(clickCount));
+}
+
+function resetBtn() {
+    'use strict';
+    var rst = document.getElementById('clickCount');
+    if (rst >= 0) {
+        document.getElementById('greetCount').innerHTML = clickCount = 0;
+    }
+    localStorage.clear();
+}
