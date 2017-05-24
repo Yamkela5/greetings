23cde9ffec11b6a3618d42e4cc5e99e9c95c6bc7
Yamkela <owamjames1@gmail.com>
2017-05-24 15:53:02 +0200

    initial commit

--- greet.js
+++ greet.js
@@ -4,14 +4,14 @@ document.getElementById('greetCount').innerHTML = clickCount;
 var namesGreeted = {};
 
 for (var i = 0; i < namesGreeted.length; i++) {
-        }
+}
 
 
 function myName() {
     'use strict';
 
     var Yash = document.getElementById('TextBox').value;
-
+//function for the English radio buttons
     if (document.getElementById('English').checked === true && Yash.length > 0 && namesGreeted[Yash] === undefined) {
         namesGreeted[Yash] = 1;
         document.getElementById('output').innerHTML = 'Hello, ' + Yash;
@@ -31,8 +31,10 @@ function myName() {
         clickCount += 1;
     } else if (document.getElementById('English').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
         document.getElementById('output').innerHTML = 'Hello, ' + Yash;
+
     } else if (document.getElementById('TshiVenda').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
         document.getElementById('output').innerHTML = 'Ndaa, ' + Yash;
+
     } else if (document.getElementById('French').checked && Yash.length > 0 && namesGreeted[Yash] !== undefined) {
         document.getElementById('output').innerHTML = 'Bonjour, ' + Yash;
     }
@@ -43,12 +45,12 @@ function myName() {
 
     localStorage.setItem('currentGreets',  Number(clickCount));
 }
-
+//the reset button
 function resetBtn() {
     'use strict';
     var rst = document.getElementById('clickCount');
     if (rst >= 0) {
-        document.getElementById('greetCount').innerHTML = clickCount = 0;
+    document.getElementById('greetCount').innerHTML = clickCount = 0;
     }
     localStorage.clear();
 }
